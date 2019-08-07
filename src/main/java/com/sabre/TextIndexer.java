243a5df7585627e4f5392e0e57f9ddb721e9fad9
dardomanski <dardomanski@gmail.com>
2019-08-07 19:35:20 +0200

    Implement TextIndexer class

--- src/main/java/com/sabre/TextIndexer.java
+++ src/main/java/com/sabre/TextIndexer.java
@@ -1,4 +1,35 @@
 package com.sabre;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class TextIndexer {
+    public Map<Character, Set<String>> indexText(String input) {
+        if (input == null) throw new InputMismatchException("Input can't be null!");
+
+        Map<Character, Set<String>> result = new LinkedHashMap<>();
+
+        input
+                .replaceAll("\\W+", "")
+                .toLowerCase()
+                .chars()
+                .distinct()
+                .mapToObj(character -> (char) character)
+                .sorted()
+                .forEach(
+                        character -> {
+                            Stream.of(input.toLowerCase().split("\\W+"))
+                                    .distinct()
+                                    .sorted()
+                                    .filter(word -> word.contains(String.valueOf(character)))
+                                    .forEach(word -> {
+                                        result.putIfAbsent(character, new TreeSet<>());
+                                        result.get(character).add(word);
+                                    });
+
+                        }
+                );
+
+        return result;
+    }
 }
